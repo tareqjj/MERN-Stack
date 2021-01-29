be743d746543ae8596ecce9e60b7f837d37b0b22
Tareq Aljamal <tareqjj@gmail.com>
2021-01-30 00:12:30 +0200

    added state and events to Prop It Up project

--- React/Class Components/prop_it_up/src/components/PersonCard.js
+++ React/Class Components/prop_it_up/src/components/PersonCard.js
@@ -1,13 +1,18 @@
 import {Component} from "react/cjs/react.production.min";
 
 class PersonCard extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {age: this.props.age}
+    }
     render() {
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={ () => this.setState({age: this.state.age + 1}) }>Increment Age by 1</button>
             </div>
         );
     }
