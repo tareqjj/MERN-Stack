f08d74b40352dce4df78c9e672ada569e81b88cc
Tareq Aljamal <tareqjj@gmail.com>
2021-02-01 18:56:56 +0200

    updated Box generator using lifitng state

--- React/Functional Components/box_generator/src/App.js
+++ React/Functional Components/box_generator/src/App.js
@@ -3,16 +3,11 @@ import {useState} from "react";
 import BoxGeneratorForm from "./components/BoxGeneratorForm";
 import DisplayBox from "./components/DisplayBox";
 
-const boxes = [];
-
 function App() {
-  const [currentColor, setCurrentColor] = useState("");
-  const [dimension, setDimension] = useState("");
+  const [currentBox, setCurrentBox] = useState([]);
 
   const addNewBox = (newColor, newDimension) => {
-    setCurrentColor( newColor );
-    setDimension( newDimension);
-    boxes.push({ newColor: newColor, newDimension: newDimension });
+    setCurrentBox( currentBox.concat( { newColor: newColor, newDimension: newDimension }) );
   }
 
   const containerStyle = {
@@ -21,12 +16,12 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <BoxGeneratorForm onNewBox={ addNewBox }/>
-      <div style={containerStyle}>
-      { boxes.map( (item, i) => <span key={i}><DisplayBox color={ item.newColor } dimension={ item.newDimension} /></span>) }
+      <div className="App">
+        <BoxGeneratorForm onNewBox={ addNewBox }/>
+        <div style={containerStyle}>
+          { currentBox.map( (item, i) => <span key={i}><DisplayBox color={ item.newColor } dimension={ item.newDimension} /></span>) }
+        </div>
       </div>
-    </div>
   );
 }
 
